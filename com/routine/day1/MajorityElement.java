ffbab82e626e0c319b5f0b3b92ea2c10c2d853e5
truongvu <truong.vun1@homecredit.vn>
2024-12-01 19:42:25 -0800

    [RTD-0003][Easy] Day 1 - Majority Element

---
+++ com/routine/day1/MajorityElement.java
@@ -0,0 +1,48 @@
+package com.routine.day1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element
+always exists in the array.
+
+Example 1:
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+Constraints:
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+ */
+
+public class MajorityElement {
+
+    public int majorityElement (int[] nums) {
+        Map<Integer, Integer> numMap            = new HashMap<>();
+        int                   majorityCondition = nums.length / 2;
+        int                   result            = 0;
+        for (int i = 0; i < nums.length; i++) {
+            numMap.put(nums[i], numMap.getOrDefault(nums[i], 0) + 1);
+            if ( numMap.containsKey(nums[i]) && numMap.get(nums[i]) > majorityCondition ) {
+                result = nums[i];
+                return result;
+            }
+        }
+        return result;
+    }
+
+    public static void main (String[] args) {
+        int[]           nums     = { 3, 2, 3 };
+        int[]           nums1    = { 2, 2, 1, 1, 1, 2, 2 };
+        MajorityElement solution = new MajorityElement();
+        System.out.println(solution.majorityElement(nums));
+        System.out.println(solution.majorityElement(nums1));
+    }
+}
